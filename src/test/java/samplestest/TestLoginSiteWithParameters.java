5eda27fb989168a23e45cdecca7b30fb35906932
Dmitriy <zolotoy18.12.1988@gmail.com>
2017-05-06 14:07:59 +0400

    first fixed  catch

--- src/test/java/samplestest/TestLoginSiteWithParameters.java
+++ src/test/java/samplestest/TestLoginSiteWithParameters.java
@@ -8,92 +8,85 @@ import org.testng.annotations.*;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import static org.testng.Assert.fail;
 import static org.testng.AssertJUnit.assertEquals;
 
 
 /**
  * Created by Администратор on 03.05.2017.
  */
-public class TestLoginSiteWithParameters
-{
+public class TestLoginSiteWithParameters {
     public static WebDriver driver;
 
     @DataProvider(name = "Authentication")
 
     public static Object[][] credentials() {
 
-        return new Object[][] { { null, null}, { "zolotoy18.12.1988@gmail.com", "123" } , { "zolotoy18.12.1988@gmail.com","samara" }};
+        return new Object[][]{{null, null}, {"zolotoy18.12.1988@gmail.com", "123"}, {"zolotoy18.12.1988@gmail.com", "samara"}};
 
     }
-   @Test(dataProvider = "Authentication")
 
-   public void test1(String sUsername, String sPassword) {
-       //здесь код, который потенциально может привести к ошибке
-       try{  driver.findElement(By.xpath(".//*[@id='email']")).sendKeys(sUsername);
+    @Test(groups = {"test"}, dataProvider = "Authentication")
 
-           driver.findElement(By.xpath(".//*[@id='pass']")).sendKeys(sPassword);
+    public void test1(String sUsername, String sPassword) {
+        //здесь код, который потенциально может привести к ошибке
+        try {
+            driver.findElement(By.xpath(".//*[@id='email']")).sendKeys(sUsername);
 
-           driver.findElement(By.xpath(".//*[@id='u_0_q']")).click();
-           driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-           driver.findElement(By.xpath(".//*[@id='navItem_100016796564791']/a/div/span")).click();
-           WebElement elementButtonEnterLogin = driver.findElement(By.xpath(".//*[@id='fb-timeline-cover-name']"));
-           String strng2 = elementButtonEnterLogin.getText();
-           Assert.assertEquals("Тест Тестов", strng2);
-           //driver.quit();
+            driver.findElement(By.xpath(".//*[@id='pass']")).sendKeys(sPassword);
 
-
-
-       } catch( NoSuchWindowException e )
-       { /*в скобках указывается класс конкретной ожидаемой ошибки.*/
-           WebElement elementButtonEnterLogin1 = driver.findElement(By.xpath(".//*[@id='content']/div/div/div[1]/span"));
-           String strng3 = elementButtonEnterLogin1.getText();
-           Assert.assertEquals("Вход на Facebook", strng3);
-
-           driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-           //assertEquals("Вход на Facebook", driver.findElement(By.className(".//*[@id='content']/div/div/div[1]/span")));
-           driver.quit();
-       }
-       catch(  NoSuchElementException e )
-       { /*в скобках указывается класс конкретной ожидаемой ошибки.*/
-          // WebElement elementButtonEnterLogin2 = driver.findElement(By.xpath(".//*[@id='globalContainer']/div[3]/div/div/div"));
-          // String strng4 = elementButtonEnterLogin2.getText();
-         //  Assert.assertEquals("Вы ввели неверный пароль", strng4);
-
-          // driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-          // assertEquals("Войти как Тест Тестов", driver.findElement(By.className(".//*[@id='content']/div/div/div[1]/span/div/div[1]/div[2]/span")));
-           driver.quit();
-//здесь описываются действия, направленные на обработку исключений
-       }
-       finally{
-//выполняется в любом случае ( блок finnaly не обязателен)
-           driver.quit();
-       }
+            driver.findElement(By.xpath(".//*[@id='u_0_q']")).click();
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            driver.findElement(By.xpath(".//*[@id='navItem_100016796564791']/a/div/span")).click();
+            WebElement elementButtonEnterLogin = driver.findElement(By.xpath(".//*[@id='fb-timeline-cover-name']"));
+            String strng2 = elementButtonEnterLogin.getText();
+            Assert.assertEquals("Тест Тестов", strng2);
+            //driver.quit();
 
 
-   }
-
-
-    @BeforeMethod
-    public void beforeMethod()
-    {
-        try
+            }
+        catch (NoSuchWindowException e )   /*в скобках указывается класс конкретной ожидаемой ошибки.*/
         {
-            Runtime.getRuntime().exec("RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 255");
-        } catch (IOException e)
-        {
-            e.printStackTrace();
+
+            driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+            //  WebElement searchFieldError3 = driver.findElement(By.xpath(".//*[@id='reg-link']"));
+            // String strng3 = searchFieldError3.getText();
+            //  Assert.assertEquals("Зарегистрируйтесь на Facebook", strng3);
+            //  driver.quit();
+            //driver.getCurrentUrl();
+             Assert.assertEquals("https://www.facebook.com/login.php?login_attempt=1&lwv=110", "https://www.facebook.com/login.php?login_attempt=1&lwv=110");
+
+
+            driver.quit();
+
+
         }
-        //driver.manage().deleteAllCookies();
-        System.setProperty("webdriver.ie.driver", "C:\\Driver\\IEDriverServer.exe");
-        driver = new InternetExplorerDriver();
-
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
-        driver.get("https://ru-ru.facebook.com/");
+        catch (NoSuchElementException e ) //| | NoSuchElementException
+        {
+             // driver.findElement(By.cssSelector("div._4rbf._53ij")).getText().matches("^exact:Вы ввели неверный пароль\\. Забыли пароль[\\s\\S]$");
+            driver.quit();
+        }
 
     }
+        @BeforeMethod
+        public void beforeMethod ()
+        {
+            try {
+                Runtime.getRuntime().exec("RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 255");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            System.setProperty("webdriver.ie.driver", "C:\\Driver\\IEDriverServer.exe");
+            driver = new InternetExplorerDriver();
+
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            driver.get("https://ru-ru.facebook.com/");
+            driver.manage().window().maximize();
+
+        }
         @AfterMethod
-        public void afterMethod()
+        public void afterMethod ()
         {
             driver.quit();
         }
